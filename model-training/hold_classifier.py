e223ab37ee0dfd3f58114a400351b754cfdaa04c
EvanMcCormick37 <e.kidmccorm@gmail.com>
2026-02-09 19:25:04 -0700

    Convert HoldClassifier for CUDA GPU training

--- model-training/hold_classifier.py
+++ model-training/hold_classifier.py
@@ -20,19 +20,35 @@ def sort_by_length(batch):
     )
 
 def train_lstm_on_packed_dataset(model: nn.Module, dataset: TensorDataset, epochs: int = 100, lr=1e-3, batch_size=256, save_path: str | None = None):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    model.train()
+    model.compile()
+
     optimizer = torch.optim.Adam(params=model.parameters(), lr=lr)
-    train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, collate_fn=sort_by_length)
+    train_loader = DataLoader(
+        dataset=dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        collate_fn=sort_by_length,
+        pin_memory=True if device.type == 'cuda' else False
+    )
     losses = []
+
     with tqdm(range(epochs)) as pbar:
         for epoch in pbar:
             epoch_losses = []
             for (idx, (x, y, lengths)) in enumerate(train_loader):
+                x, y = x.to(device), y.to(device)
                 packed_input = pack_padded_sequence(
                     input = x,
                     lengths = lengths,
                     batch_first = True,
                     enforce_sorted = True
                 )
+
+                # Model prediction and loss update
+                optimizer.zero_grad()
                 pred_logits = model(packed_input)
                 loss = model.loss(pred_logits, y)
                 loss.backward()
@@ -41,7 +57,7 @@ def train_lstm_on_packed_dataset(model: nn.Module, dataset: TensorDataset, epoch
                 epoch_losses.append(loss.item())
             mean_loss = sum(epoch_losses)/len(epoch_losses)
             losses.append(mean_loss)
-            pbar.set_postfix_str(f"Epoch: {epoch}, Mean Loss: {mean_loss}, Min Loss: {min(losses)}, Batches{len(epoch_losses)}")
+            pbar.set_postfix_str(f"Epoch: {epoch}, Mean Loss: {mean_loss}, Min Loss: {min(losses)}, {len(epoch_losses)} Batches")
     
     if save_path:
         torch.save(model.state_dict(), save_path)
@@ -60,12 +76,14 @@ def train_lstm_on_packed_dataset(model: nn.Module, dataset: TensorDataset, epoch
 class HoldClassifier(nn.Module):
     def __init__(self, input_dim: int = 8, hidden_dim: int = 256, num_layers: int = 1):
         super().__init__()
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.lstm = nn.LSTM(
             input_size=input_dim,
             hidden_size=hidden_dim,
             num_layers=num_layers,
             dropout = 0.3,
-            bidirectional=True
+            bidirectional=True,
+            device=self.device
         )
 
         self.classification_head = nn.Sequential(
