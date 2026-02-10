308ea71b06f6d6bfa1279e4bf61b3ccfc0884f79
EvanMcCormick37 <e.kidmccorm@gmail.com>
2026-02-10 16:30:56 -0700

    fd\

--- model-training/hold_classifier.py
+++ model-training/hold_classifier.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+import torch.nn.functional as F
 from torch import Tensor
 from torch.utils.data import TensorDataset, DataLoader
 from torch.nn.utils.rnn import pack_padded_sequence, PackedSequence
