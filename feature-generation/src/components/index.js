f75967b5da8d81c74c431ad0011bce12e8fc9020
EvanMcCormick37 <e.kidmccorm@gmail.com>
2026-03-08 18:11:54 -0600

    Removed Legacy feature-generation and added an ARCHITECTURE.md file.

--- feature-generation/src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Toolbar } from './Toolbar';
-export { default as CanvasArea } from './CanvasArea';
-export { default as AlignmentPanel } from './AlignmentPanel';
-export { default as HelpPanel } from './HelpPanel';
-export { default as ClimbsPanel } from './ClimbsPanel';
-export { default as MovesPanel } from './MovesPanel';
